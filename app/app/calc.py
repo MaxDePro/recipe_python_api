c2b5496d2688e0524ee5e03299a0a2b8c100816c
MaxDePro <xmaksg@gmail.com>
2022-11-27 14:32:13 +0200

    command class modofied and passed flake 8 tests

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,9 +1,11 @@
-""" 
+"""
 Simple calculator
 """
 
+
 def sum_num(x, y):
     return x + y
+
 
 def substract_nums(x, y):
     return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,18 +1,20 @@
 """
 Simplae testing for calculator
 """
+
 from django.test import SimpleTestCase
 
 from . import calc
+
 
 class CalcTesting(SimpleTestCase):
     """ Test the calc model """
     def test_calc_sum(self):
         res = calc.sum_num(5, 6)
-        
+
         self.assertEqual(res, 11)
-        
+
     def test_calc_substract(self):
         res = calc.substract_nums(10, 15)
-        
+
         self.assertEqual(res, 5)
