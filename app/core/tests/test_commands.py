97a930230b47953fb561113fb35a1486a3c4e199
MaxDePro <xmaksg@gmail.com>
2022-12-01 11:58:30 +0200

    created tests and serializer method for users/view class

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -13,10 +13,10 @@ from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    """Test commands"""
+    """Test commands."""
 
     def test_wait_for_db_ready(self, patched_check):
-        """Test waiting for database if database is ready."""
+        """Test waiting for database if database ready."""
         patched_check.return_value = True
 
         call_command('wait_for_db')
@@ -25,7 +25,7 @@ class CommandTests(SimpleTestCase):
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        """Test for database when getting eporational error"""
+        """Test waiting for database when getting Operationalerror"""
         patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
 
