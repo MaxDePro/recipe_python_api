a9d0462bc20e82dc84f74f002843811cd050fdb0
MaxDePro <xmaksg@gmail.com>
2022-12-01 12:50:31 +0200

    little fixed flake8 errors

--- app/core/admin.py
+++ app/core/admin.py
@@ -7,19 +7,21 @@ from django.utils.translation import gettext_lazy as _
 
 from core import models
 
+
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permisions'),
             {
                 'fields': (
                     'is_active',
                     'is_staff',
-                    'is_superuser'
+                    'is_superuser',
                 )
             }
         ),
@@ -28,18 +30,18 @@ class UserAdmin(BaseUserAdmin):
     readonly_fields = ['last_login']
     add_fieldsets = (
         (None, {
-        'classes': ('wide',),
-        'fields': (
-            'email',
-            'password1',
-            'password2',
-            'name',
-            'is_active',
-            'is_staff',
-            'is_superuser',
-            )
-        }),
-    )
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                ),
+            }),
+        )
 
 
 admin.site.register(models.User, UserAdmin)
