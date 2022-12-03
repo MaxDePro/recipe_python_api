08640aa0f4f1ad21ed18c46857d24fc7bae60f1d
MaxDePro <xmaksg@gmail.com>
2022-12-03 22:48:31 +0200

    checks recipe detail serialization class

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,7 +1,6 @@
 """
-Serializers for recipe APIs.
+Serializers for recipe APIs
 """
-
 from rest_framework import serializers
 
 from core.models import Recipe
@@ -13,6 +12,11 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
-        read_only_field = ['id']
+        read_only_fields = ['id']
 
-    
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for the detail view."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
