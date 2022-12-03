efd00fd32dda945b669a993196372ed63f6d5f74
MaxDePro <xmaksg@gmail.com>
2022-12-03 19:36:02 +0200

    added recipe tests and views and serializers

--- app/user/views.py
+++ app/user/views.py
@@ -23,11 +23,11 @@ class CreateTokenView(ObtainAuthToken):
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
-    """Manage the authenticated users."""
+    """Manage the authenticated user."""
     serializer_class = UserSerializer
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_objects(self):
+    def get_object(self):
         """Retrieve and return the authenticated user."""
         return self.request.user
